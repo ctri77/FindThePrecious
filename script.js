a6b636acc24a9fd908ae65dc7b739f5d9daeb50d
ctri77 <40467084+ctri77@users.noreply.github.com>
2018-09-23 10:40:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+
 $('.navbar-collapse .navbar-nav > .nav-item > .nav-link').click(function(){
     $('.collapse.show').removeClass('show').css('height', '0');
-    });
-    $('#btn-send').click(function() {
-	alert("Your message has been seen. We will contact you as soon as possible !");
+});
+$('#btn-send').click(function() {
+    alert("Your message has been seen. We will contact you as soon as possible !");
 });
