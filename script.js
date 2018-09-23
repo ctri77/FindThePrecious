cc2755bd391c786308ac5bebc84258a5dd00e8c8
ctri77 <40467084+ctri77@users.noreply.github.com>
2018-09-23 10:25:31 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+	  	$('.navbar-collapse .navbar-nav > .nav-item > .nav-link').click(function(){
+	    $('.collapse.show').removeClass('show').css('height', '0');
+	    });
+	  	$('#btn-send').click(function() {
+	  		alert("Your message has been seen. We will contact you as soon as possible !");
+	  	});
